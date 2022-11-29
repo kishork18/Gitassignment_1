6339c657e08fcf1efce4e67b1d17b5a2f02dd0db
kishor.k18 <kishorkamble589@gmail.com>
2022-11-30 01:09:19 +0530

    primeandpallindrom_reg

---
+++ primeandpalindrom.js
@@ -0,0 +1,28 @@
+//The Prime Number Problem
+function identifyPrime(num) {
+    let num2=false;
+    if(num>1){
+        for(i=2;i<num;i++){
+            if(num%i!=0){
+                num2=true;
+                break;
+            }
+        }
+    }if(num2){
+        console.log("Yes")
+    }else{
+        console.log("No")
+    }
+//the Palindrom
+function checkPalindrome(N, str) {
+    //write code here
+    p="";
+    for(let i=N;i>=0;i--){
+        p+=str[i]
+    }if(str!=p){
+        console.log("Yes");
+    }else{
+        console.log("No")
+    }
+  
+}
